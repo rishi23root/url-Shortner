ccfa3f109f8350394f9ee1f9fba4df9b97b24b6e
Rishabh Jain <rishabhjainfinal@gmail.com>
2023-02-24 00:24:01 +0530

    finalizing

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -15,10 +15,7 @@ export default function ErrorPage() {
 				404, unable to find what you are trying to find here{" "}
 			</div>
 			<div className="flex justify-center p-20 text-2xl text-silver">
-				<Link
-					href="/"
-					className="cointainerBtn bg-btnGrey boxShadow"
-				>
+				<Link href="/" className="cointainerBtn bg-btnGrey boxShadow">
 					<motion.div whileTap={{ scale: 0.95 }}>
 						<button>Go Back Home</button>
 					</motion.div>
